2eed36251724901d184a6647a577bf7e8c515d6e
CasasDavid <davidcasas2506@gmail.com>
2023-12-10 14:31:54 -0500

    Para no hacer log in cada vez

--- Program/main.py
+++ Program/main.py
@@ -3,17 +3,18 @@ import faulthandler
 faulthandler.enable()
 from NutritionProgram import dashboard, home_screen
 
-home_window = home_screen.HomeScreen(
-    width=500, height=500, appearance_mode="light", color_theme="green"
-)
-home_window.show_homescreen()
+# home_window = home_screen.HomeScreen(
+#     width=500, height=500, appearance_mode="light", color_theme="green"
+# )
+# home_window.show_homescreen()
 
 dash_board = dashboard.Dashboard(
     width=1380,
     height=720,
     appearance="dark",
     theme_color="green",
-    userName=home_window.get_user_enrollment_id(),
+    # userName=home_window.get_user_enrollment_id(),
+    userName="Davidlcasas"
 )
 
 dash_board.show_dashboard()
